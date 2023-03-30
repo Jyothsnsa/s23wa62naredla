129be8d65ce467a3b9ab30a65cb9751f71609904
Jyothsnsa <123060113+Jyothsnsa@users.noreply.github.com>
2023-03-29 20:48:49 -0500

    bonus

---
+++ routes/computation.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+let x=Math.floor(Math.random());
+let y
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    if(req.query.x!=undefined){
+        x=req.query.x;
+
+    }
+    let r=Math.pow(x);
+    let t=Math.sign(x);
+    let n=Math.tanh(x);
+    
+  res.render('computation', { x:x,pow:r, sign:t, tanh:n});
+});
+
+module.exports = router;
